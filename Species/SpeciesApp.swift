8cefff6228b4353ec1a4152453d231457b7622dd
rparham1997 <rparham1997@gmail.com>
2023-07-21 01:51:31 -0400

    Initial Commit

---
+++ Species/SpeciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeciesApp.swift
+//  Species
+//
+//  Created by Ramar Parham on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct SpeciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
